6825d7870455610c8813515b89c904f92657337d
nanassyd <nanassy.dani@icloud.com>
2023-09-24 10:13:35 +0200

    hibak javitva

--- main.cpp
+++ main.cpp
@@ -4,18 +4,18 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[N_ELEMENTS];						//typo
+    std::cout << "1-100 ertekek duplazasa" << std::endl;//missing ;
+    for (int i = 0; i < N_ELEMENTS; i++)				//missing statement
     {
-        b[i] = i * 2;
+        b[i] = (i+1) * 2;								//should be +
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i < N_ELEMENTS; i++)				//missing statement
     {
-        std::cout << "Ertek:"
-    }    
+        std::cout << "Ertek:" << b[i] <<std::endl;		//i output missing
+    }
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
+    float atlag;										//should be double
     for (int i = 0; i < N_ELEMENTS, i++)
     {
         atlag += b[i]
