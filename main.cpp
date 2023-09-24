ef9c2ca99c4defb8a3ca5b9d53f72e5a5bfb62c0
johncodesalot <nagy.jan95@gmail.com>
2023-09-24 13:57:20 +0200

    First contact

--- main.cpp
+++ main.cpp
@@ -22,5 +22,6 @@ int main()
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
+    std::cout << "Hello there!";                        //edited by johncodesalot
     return 0;
 }
